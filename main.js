862c4d726327f6ad21117fd1eec4da1f2eef8053
jennyvanessaquinto <vane.quinto19@gmail.com>
2022-09-23 01:40:53 -0500

    The Challenge Portfolio

---
+++ main.js
@@ -0,0 +1,74 @@
+const homeNav = document.getElementById('home');
+const porfolioNav = document.getElementById('portfolio');
+const aboutNav = document.getElementById('about');
+const contactNav = document.getElementById('contact');
+const navigationMenu = document.getElementById('navigationMenu');
+const homePage = document.getElementById('homePage');
+const porfolioPage = document.getElementById('porfolioPage');
+const aboutPage = document.getElementById('aboutPage');
+const contactPage= document.getElementById('contactPage');
+
+const menuBtn = document.querySelector('.menu-btn');
+let menuOpen = false;
+const pageLocation = Document.currentScript;
+console.log(document.location.href);
+menuBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    if (!menuOpen) {
+        menuBtn.classList.add('open');        
+        navigationMenu.classList.remove('hidden');       
+        menuOpen = true;
+        if(document.location.href=="http://127.0.0.1:5500/index.html"){
+            homeNav.classList.remove('hidden');
+        }
+        if(document.location.href=="http://127.0.0.1:5500/pages/about.html"){
+            aboutNav.classList.remove('hidden');
+        }
+        if(document.location.href=="http://127.0.0.1:5500/pages/contact.html"){
+            contactNav.classList.remove('hidden');
+        }
+        if(document.location.href=="http://127.0.0.1:5500/pages/portfolio.html"){
+            porfolioNav.classList.remove('hidden');
+        }
+        
+    } else {
+        menuBtn.classList.remove('open');
+        navigationMenu.classList.add('hidden');
+        menuOpen = false;
+    }
+});
+
+(function runForever() {
+    var m = 0, text;
+    text = "VANESSA QUINTO";
+    function typing() {
+        if (m < text.length) {
+            document.getElementById("texth1").
+                innerHTML += text.charAt(m);
+            m++;
+            setTimeout(typing, 200);
+        }
+    }
+    typing();
+})()
+
+homePage.addEventListener('click',(e)=>{
+    e.preventDefault();
+    window.location.assign("/index.html");
+    
+});
+
+aboutPage.addEventListener('click',(e)=>{
+    e.preventDefault();
+    window.location.assign("/pages/about.html");
+    
+});
+porfolioPage.addEventListener('click',(e)=>{
+    e.preventDefault();
+    window.location.assign("/pages/portfolio.html");
+    
+});
+contactPage.addEventListener('click',(e)=>{
+    e.preventDefault();
+    window.location.assign("/pages/contact.html");   
+});
---
+++ script/portfolio.js
@@ -0,0 +1,72 @@
+const homeNav = document.getElementById('home');
+const porfolioNav = document.getElementById('portfolio');
+const aboutNav = document.getElementById('about');
+const contactNav = document.getElementById('contact');
+const navigationMenu = document.getElementById('navigationMenu');
+const homePage = document.getElementById('homePage');
+const porfolioPage = document.getElementById('porfolioPage');
+const aboutPage = document.getElementById('aboutPage');
+const contactPage= document.getElementById('contactPage');
+
+const menuBtn = document.querySelector('.menu-btn');
+let menuOpen = false;
+const pageLocation = Document.currentScript;
+console.log(document.location.href);
+menuBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    if (!menuOpen) {
+        menuBtn.classList.add('open');        
+        navigationMenu.classList.remove('hidden');       
+        menuOpen = true;
+        if(document.location.href=="http://127.0.0.1:5500/index.html"){
+            homeNav.classList.remove('hidden');
+        }
+        if(document.location.href=="http://127.0.0.1:5500/pages/about.html"){
+            aboutNav.classList.remove('hidden');
+        }
+        if(document.location.href=="http://127.0.0.1:5500/pages/contact.html"){
+            contactNav.classList.remove('hidden');
+        }
+        if(document.location.href=="http://127.0.0.1:5500/pages/portfolio.html"){
+            porfolioNav.classList.remove('hidden');
+        }
+        
+    } else {
+        menuBtn.classList.remove('open');
+        navigationMenu.classList.add('hidden');
+        menuOpen = false;
+    }
+});
+homePage.addEventListener('click',(e)=>{
+    e.preventDefault();
+    window.location.assign("/index.html");
+    
+});
+
+aboutPage.addEventListener('click',(e)=>{
+    e.preventDefault();
+    window.location.assign("/pages/about.html");
+    
+});
+porfolioPage.addEventListener('click',(e)=>{
+    e.preventDefault();
+    window.location.assign("/pages/portfolio.html");
+    
+});
+contactPage.addEventListener('click',(e)=>{
+    e.preventDefault();
+    window.location.assign("/pages/contact.html");   
+});
+
+document.addEventListener("click", ({ target }) => {
+    
+    if (target.classList.contains("1")) {
+        window.location.assign("https://jennyvanessaquinto.github.io/Sprint1/");       
+    }
+    if (target.classList.contains("2")) {
+        window.location.assign("https://jennyvanessaquinto.github.io/Sprint2/");      
+    }
+    if (target.classList.contains("3")) {
+        window.location.assign("https://jennyvanessaquinto.github.io/ProyectoNivelatorio/");       
+    }
+});
